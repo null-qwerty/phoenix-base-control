f4cd5d5ecd667357ad56fcc9919c878ed45b9866
Haoyu Chen <null-qwerty@outlook.com>
2026-03-21 17:50:20 +0800

    style(status): add comments

--- core/status.hpp
+++ core/status.hpp
@@ -3,17 +3,22 @@
 #include <cstdint>
 
 enum EsfStatus : uint8_t {
-    ESF_SUCCESS = 0,
-    ESF_TIMEOUT,
-    ESF_MULTIPLE_ERROR,
-    ESF_QUEUE_FULL,
-    ESF_QUEUE_EMPTY,
-    ESF_QUEUE_PUSH_ERROR,
-    ESF_QUEUE_POP_ERROR,
-    ESF_CONNECTIVITY_ERROR,
-    ESF_CONNECTIVITY_SEND_ERROR,
-    ESF_CONNECTIVITY_RECEIVE_ERROR,
-    ESF_MOTOR_HELPER_REGISTER_ERROR,
-    ESF_MOTOR_NOT_REGISTERD_TO_HELPER,
-    ESF_MOTOR_HELPER_DATA_INVALIED,
+    /* 通用错误 */
+    ESF_SUCCESS = 0,    // 正常
+    ESF_TIMEOUT,        // 超时
+    ESF_NULLPTR_ERROR,  // 传入空指针
+    ESF_MULTIPLE_ERROR, // 多种错误
+    /* 队列相关错误 */
+    ESF_QUEUE_FULL,       // 待加入元素的队列已满
+    ESF_QUEUE_EMPTY,      // 待弹出元素的队列已空
+    ESF_QUEUE_PUSH_ERROR, // 加入元素时出现错误
+    ESF_QUEUE_POP_ERROR,  // 弹出元素时出现错误
+    /* 通信相关错误 */
+    ESF_CONNECTIVITY_ERROR,         // 通信出现错误
+    ESF_CONNECTIVITY_SEND_ERROR,    // 发送消息时出现错误
+    ESF_CONNECTIVITY_RECEIVE_ERROR, // 接收消息时出现错误
+    /* 电机相关错误 */
+    ESF_MOTOR_HELPER_REGISTER_ERROR,   // 电机注册到 helper 时出现错误
+    ESF_MOTOR_NOT_REGISTERD_TO_HELPER, // 电机未注册到该 helper
+    ESF_MOTOR_HELPER_DATA_INVALIED,    // (存在通信协议校验的情况下) helper 接收到的电机反馈帧校验未通过
 };
